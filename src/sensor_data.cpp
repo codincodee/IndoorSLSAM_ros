1e3019abbbb0c45c3127e464af726f56d95a60d3
codincodee <codincodee@outlook.com>
2018-03-06 14:50:54 +0800

    Align with the redefination of slsam::Scan2D

--- src/sensor_data.cpp
+++ src/sensor_data.cpp
@@ -12,22 +12,15 @@ std::shared_ptr<slsam::Scan2D> Scan2D::ToSlsamScan() const {
     return shared_ptr<slsam::Scan2D>();
   }
   std::shared_ptr<slsam::Scan2D> slsam_scan(new slsam::Scan2D);
-  slsam_scan->points.reserve(scan->ranges.size());
-  int i = -1;
-  auto angle_min = scan->angle_min;
-  auto angle_increment = scan->angle_increment;
-  
-  for (auto& range : scan->ranges) {
-    ++i;
-    if (range > scan->range_max || range < scan->range_min) {
-      continue;
-    }
-    slsam::Point2 point;
-    auto angle = angle_min + angle_increment * i;
-    point.x = range * sin(angle);
-    point.y = range * cos(angle);
-    slsam_scan->points.push_back(point);
-  }
+  slsam_scan->angle_min = scan->angle_min;
+  slsam_scan->angle_max = scan->angle_max;
+  slsam_scan->angle_increment = scan->angle_increment;
+  slsam_scan->time_increment = scan->time_increment;
+  slsam_scan->scan_time = scan->scan_time;
+  slsam_scan->range_min = scan->range_min;
+  slsam_scan->range_max = scan->range_max;
+  slsam_scan->ranges = scan->ranges;
+  slsam_scan->intensities = scan->intensities;
   return slsam_scan;
 }
 } // namespace slsam_ros
