19ea4f804e4ef5f84de018acc3e78a1ab04cafe7
codincodee <codincodee@outlook.com>
2018-03-06 16:41:48 +0800

    Parsed scan stamp

--- src/sensor_data.cpp
+++ src/sensor_data.cpp
@@ -12,6 +12,7 @@ std::shared_ptr<slsam::Scan2D> Scan2D::ToSlsamScan() const {
     return shared_ptr<slsam::Scan2D>();
   }
   std::shared_ptr<slsam::Scan2D> slsam_scan(new slsam::Scan2D);
+  slsam_scan->stamp = slsam::time_now() - slsam::NSec((ros::Time::now() - scan->header.stamp).toNSec());
   slsam_scan->angle_min = scan->angle_min;
   slsam_scan->angle_max = scan->angle_max;
   slsam_scan->angle_increment = scan->angle_increment;
